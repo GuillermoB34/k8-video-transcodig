110ccd784def08adebcdedffe74eab76291b47af
MrHide23 <mr.hidemagarium@gmail.com>
2026-05-29 17:39:54 +0200

    Improved Curl recursive

--- p2/curl-recursivo-peticiones.sh
+++ p2/curl-recursivo-peticiones.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+for i in {1..10}
+do
 curl -v "http://192.168.56.3:30080/x264-fn-events" \
     -X POST \
-    -H "Ce-Id: 1" \
+    -H "Ce-Id: $(echo $i)" \
     -H "Ce-specversion: 1.0" \
     -H "Ce-Type: x264" \
     -H "Ce-Source: curl" \
@@ -10,8 +13,10 @@ curl -v "http://192.168.56.3:30080/x264-fn-events" \
     "sourceUrl":"https://routerdi1315.uv.es/video/video.mp4",
     "crf": 1,
     "frameRate":"23.98",
-    "videoNameDestination":"video-crf-1.mp4",
+    "videoNameDestination":"video-crf-'$i'.mp4",
     "destUrl":"http://svc-upload-nginx:8080/upload",
     "bucketParamName":"bucket",
     "bucketParamValue":"encodedVideos"
     }'
+
+done
